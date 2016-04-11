a30d8616e3ffb086480de331d047197b061691d5
Rob <rvbaast@users.noreply.github.com>
2016-04-11 13:47:16 +0200

    Added support for generating pending only scripts (#29)
    * Added support for generating pending only scripts eg:
    migrate script pending
    migrate script pending_undo

--- src/main/java/org/apache/ibatis/migration/commands/ScriptCommand.java
+++ src/main/java/org/apache/ibatis/migration/commands/ScriptCommand.java
@@ -26,6 +26,7 @@ import java.util.StringTokenizer;
 import org.apache.ibatis.migration.Change;
 import org.apache.ibatis.migration.MigrationException;
 import org.apache.ibatis.migration.operations.DatabaseOperation;
+import org.apache.ibatis.migration.operations.StatusOperation;
 import org.apache.ibatis.migration.options.SelectedOptions;
 
 public final class ScriptCommand extends BaseCommand {
@@ -41,23 +42,44 @@ public final class ScriptCommand extends BaseCommand {
         throw new MigrationException("The script command requires a range of versions from v1 - v2.");
       }
       StringTokenizer parser = new StringTokenizer(sparams[0]);
-      if (parser.countTokens() != 2) {
+      int tokenCount = parser.countTokens();
+      boolean scriptPending = false;
+      boolean scriptPendingUndo = false;
+
+      String firstToken = parser.nextToken();
+
+      if(tokenCount == 1 && firstToken.equals("pending")){
+        scriptPending = true;
+      } else if(tokenCount == 1 && firstToken.equals("pending_undo")) {
+        scriptPendingUndo = true;
+      }
+
+      else if (!scriptPending&& !scriptPendingUndo && tokenCount != 2) {
         throw new MigrationException("The script command requires a range of versions from v1 - v2.");
       }
-      BigDecimal v1 = new BigDecimal(parser.nextToken());
-      BigDecimal v2 = new BigDecimal(parser.nextToken());
-      int comparison = v1.compareTo(v2);
-      if (comparison == 0) {
-        throw new MigrationException("The script command requires two different versions. Use 0 to include the first version.");
+
+      BigDecimal v1 = (scriptPending || scriptPendingUndo) ? null : new BigDecimal(firstToken);
+      BigDecimal v2 = (scriptPending || scriptPendingUndo) ? null :new BigDecimal(parser.nextToken());
+
+      boolean undo;
+      undo = scriptPendingUndo;
+      if(!scriptPending && !scriptPendingUndo) {
+        int comparison = v1.compareTo(v2);
+        if (comparison == 0) {
+          throw new MigrationException("The script command requires two different versions. Use 0 to include the first version.");
+        }
+        undo = comparison > 0;
       }
-      boolean undo = comparison > 0;
-      List<Change> migrations = getMigrationLoader().getMigrations();
+
+      List<Change> migrations =  (scriptPending || scriptPendingUndo) ?
+              new StatusOperation().operate(getConnectionProvider(), getMigrationLoader(), getDatabaseOperationOption(), null).getCurrentStatus() :
+              getMigrationLoader().getMigrations();
       Collections.sort(migrations);
       if (undo) {
         Collections.reverse(migrations);
       }
       for (Change change : migrations) {
-        if (shouldRun(change, v1, v2)) {
+        if (shouldRun(change, v1, v2, scriptPending || scriptPendingUndo)) {
           printStream.println("-- " + change.getFilename());
           Reader migrationReader = getMigrationLoader().getScriptReader(change, undo);
           char[] cbuf = new char[1024];
@@ -91,12 +113,17 @@ public final class ScriptCommand extends BaseCommand {
     return "DELETE FROM " + changelogTable() + " WHERE ID = " + change.getId() + getDelimiter();
   }
 
-  private boolean shouldRun(Change change, BigDecimal v1, BigDecimal v2) {
-    BigDecimal id = change.getId();
-    if (v1.compareTo(v2) > 0) {
-      return (id.compareTo(v2) > 0 && id.compareTo(v1) <= 0);
-    } else {
-      return (id.compareTo(v1) > 0 && id.compareTo(v2) <= 0);
+  private boolean shouldRun(Change change, BigDecimal v1, BigDecimal v2, boolean pendingOnly) {
+    if(!pendingOnly) {
+      BigDecimal id = change.getId();
+      if (v1.compareTo(v2) > 0) {
+        return (id.compareTo(v2) > 0 && id.compareTo(v1) <= 0);
+      } else {
+        return (id.compareTo(v1) > 0 && id.compareTo(v2) <= 0);
+      }
+    }
+    else {
+      return change.isPending();
     }
   }
   
