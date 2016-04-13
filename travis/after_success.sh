b4e2f982b6e504a13a788fc0d8cbc8e02805901b
Jeremy Landis <jeremylandis@hotmail.com>
2016-04-12 22:49:42 -0400

    [license] Update parent to 27 and let it fix license headers

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#
+#    Copyright 2010-2016 the original author or authors.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+
 
 # Get Project Repo
 mybatis_repo=$(git config --get remote.origin.url 2>&1)
@@ -30,7 +46,7 @@ if [ "$mybatis_repo" == "https://github.com/mybatis/migrations.git" ] && [ "$TRA
   if [ $VER == "16" ]; then
     mvn clean deploy -q --settings ./travis/settings.xml
     echo -e "Successfully deployed SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
-  elif [ $VER == "17" ]; then
+  elif [ $VER == "18" ]; then
     mvn clean test jacoco:report coveralls:report -q
     echo -e "Successfully ran coveralls under Travis job ${TRAVIS_JOB_NUMBER}"
 	# various issues exist currently in building this so comment for now
