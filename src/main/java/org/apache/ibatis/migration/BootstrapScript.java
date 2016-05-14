e4a1ea18fa4aa969505f77cd03d67c87b959b354
Iwao AVE! <harawata@gmail.com>
2016-05-14 21:20:51 +0900

    Add OnAbort script executed when migration failed. Mainly to display error when creating procedure etc. failed on Oracle (see #46). (#47)

--- src/main/java/org/apache/ibatis/migration/BootstrapScript.java
+++ src/main/java/org/apache/ibatis/migration/BootstrapScript.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2010-2015 the original author or authors.
+ *    Copyright 2010-2016 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -15,6 +15,5 @@
  */
 package org.apache.ibatis.migration;
 
-public interface BootstrapScript {
-  String getScript();
+public interface BootstrapScript extends SimpleScript {
 }
