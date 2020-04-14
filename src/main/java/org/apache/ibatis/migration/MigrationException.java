e9c208b3bb67c00cfcae2014c531f3b72b23ec46
Iwao AVE! <harawata@gmail.com>
2020-04-15 00:51:20 +0900

    Remove PersistenceException reference

--- src/main/java/org/apache/ibatis/migration/MigrationException.java
+++ src/main/java/org/apache/ibatis/migration/MigrationException.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2010-2017 the original author or authors.
+ *    Copyright 2010-2020 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -15,9 +15,7 @@
  */
 package org.apache.ibatis.migration;
 
-import org.apache.ibatis.exceptions.PersistenceException;
-
-public class MigrationException extends PersistenceException {
+public class MigrationException extends RuntimeException {
 
   private static final long serialVersionUID = 491769430730827896L;
 
